13d459cfa1e4e7acc7eff926895072ac0e868061
Rodrigo Tiscareno <rodrigotiscareno1@gmail.com>
2023-10-18 00:15:59 -0400

    GNS

--- gns.py
+++ gns.py
@@ -1,46 +1,50 @@
-from concurrent.futures import ProcessPoolExecutor
 from vns import variable_neighborhood_search
-
-
-def _vns_helper(args):
-    return variable_neighborhood_search(*args)
+import sys
 
 
 def generalized_neighborhood_search(
     dimensions,
     x_range,
-    neighborhoods,
+    regions,
     local_search_maximum_iterations,
     vns_maximum_neighborhood_iterations,
+    vns_neighborhoods,
     convergence_threshold,
 ):
-    absolute = abs(x_range[0]) + abs(x_range[1])
-    splits = absolute / neighborhoods
+    absolute = abs(x_range[0] - x_range[1])
+    splits = absolute / regions
+    print(absolute)
 
-    defined_neighborhoods = [
-        [x_range[0] + splits * i, x_range[0] + splits * (i + 1)]
-        for i in range(neighborhoods)
+    defined_regions = [
+        [x_range[0] + splits * i, x_range[0] + splits * (i + 1)] for i in range(regions)
     ]
 
-    inputs = [
+    x_history = []
+    cost_history = []
+    best_solution = sys.maxsize
+    best_x = []
+
+    for region in defined_regions:
+        print(region)
         (
+            current_x,
+            current_solution,
+            x,
+            y,
+        ) = variable_neighborhood_search(
             dimensions,
-            neighborhood_range,
-            neighborhoods,
+            region,
+            vns_neighborhoods,
             local_search_maximum_iterations,
             vns_maximum_neighborhood_iterations,
             convergence_threshold,
         )
-        for neighborhood_range in defined_neighborhoods
-    ]
 
-    with ProcessPoolExecutor(max_workers=len(defined_neighborhoods)) as executor:
-        results = list(executor.map(_vns_helper, inputs))
+        if current_solution < best_solution:
+            best_solution = current_solution
+            best_x = current_x
 
-    print(results)
+        cost_history.append(current_solution)
+        x_history.append(current_x)
 
-    return results
-
-
-if __name__ == "__main__":
-    results = generalized_neighborhood_search(2, [-500, 500], 2, 40, 10, 0.01)
+    return best_x, best_solution, x_history, cost_history
